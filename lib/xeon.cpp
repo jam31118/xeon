048ba45cf7a9be01228a13903d066268e6d2bdff
Kang Woosung <fredrickang@naver.com>
2017-04-25 23:37:45 +0900

    'testing'

--- lib/xeon.cpp
+++ lib/xeon.cpp
@@ -67,3 +67,5 @@ void IFstage(struct XeonStruct *Xeon) {
 	//cout << "Xeon.PC == " << Xeon->PC << endl;
 	//cout << "Xeon.ID_EX.ConSig.EX == " << Xeon->ID_EX.ConSig.EX << endl;
 }
+
+/*Testing GIT*/
